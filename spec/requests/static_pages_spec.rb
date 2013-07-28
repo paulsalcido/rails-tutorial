65a5d91f89cd825ac0f604ea6de1f61b15e5ea20
Paul David Salcido <paulsalcido.79@gmail.com>
2013-07-28 01:19:05 +0000

    Static routes converted to named routes.
    The routes for contact, help, home and about have been converted into
    something much more relevant - /home, /help, etc., rather than directly
    to static_pages/...html.
    
    This important concept is one that I'll have to make sure I spend plenty
    of time trying to remember.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,53 +5,53 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe 'Home page' do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit home_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit home_path
       page.should have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit "/static_pages/home"
+      visit home_path
       page.should_not have_title('| Home')
     end
   end
 
   describe 'Help page' do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_title("#{base_title} | Help")
     end
   end
 
   describe 'About page' do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_title("#{base_title} | About Us")
     end
   end
 
   describe 'Contact' do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_title("#{base_title} | Contact")
     end
   end
