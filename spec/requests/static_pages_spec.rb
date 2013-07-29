32242a143d2309dd4fb47909acb4ceba78db8d7c
Paul David Salcido <paulsalcido.79@gmail.com>
2013-07-29 00:37:21 +0000

    Simplification of static page tests.
    This marks the completion of the exercises for chapter five of the Ruby
    on Rails tutorial, or at least as much as I'm going to do.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,51 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(page_title) }
+  end
+
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe 'Home page' do
     before { visit home_path }
-
-    it { should have_selector('h1', :text => 'Sample App') }
-    it { should have_title("#{base_title}") }
+    let (:heading)    { 'Sample App' }
+    let (:page_title) { "#{base_title}" }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe 'Help page' do
     before { visit help_path }
-
-    it { should have_selector('h1', :text => 'Help') }
-    it { page.should have_title("#{base_title} | Help") }
+    let (:heading)    { 'Help' }
+    let (:page_title) { "#{base_title} | Help" }
+    it_should_behave_like "all static pages"
   end
 
   describe 'About page' do
     before { visit about_path }
-
-    it { should have_selector('h1', :text => 'About Us') }
-    it { should have_title("#{base_title} | About Us") }
+    let (:heading)    { 'About Us' }
+    let (:page_title) { "#{base_title} | About Us" }
+    it_should_behave_like "all static pages"
   end
 
   describe 'Contact' do
     before { visit contact_path }
+    let (:heading)    { 'Contact' }
+    let (:page_title) { "#{base_title} | Contact" }
+    it_should_behave_like "all static pages"
+  end
 
-    it { should have_selector('h1', :text => 'Contact') }
-    it { should have_title("#{base_title} | Contact") }
+  it "should have the right links on the layout" do
+    visit root_path
+    first(:link, "About").click
+    page.should have_title("#{base_title} | About Us")
+    first(:link, "Help").click
+    page.should have_title("#{base_title} | Help")
+    first(:link, "Contact").click
+    page.should have_title("#{base_title} | Contact")
+    first(:link, "Home").click
+    first(:link, "Sign up").click
+    page.should have_title("#{base_title} | Sign up")
   end
 end
