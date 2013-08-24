f8d4c46500fd349ddccd4e2aaea7a4dcb433c703
Paul David Salcido <paulsalcido.79@gmail.com>
2013-08-24 22:18:17 +0000

    Edit user pages
    Users are now able to edit their user information, and it is limited by
    the users that actually own the information.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,15 +10,30 @@ module SessionsHelper
     @current_user = user
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
   def signed_in?
     !current_user.nil?
   end
 
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
-    debug remember_token
     @current_user ||= User.find_by(remember_token: remember_token)
-    debug @current_user
-    @current_user
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,6 +30,68 @@ describe "AuthenticationPages" do
       it { should have_link("Profile",      href: user_path(user)) }
       it { should have_link("Sign out",     href: signout_path) }
       it { should_not have_link("Sign in",  href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
+  end
+
+  describe "authorization" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:wrong_user) { FactoryGirl.create(:user, email: "wrong+user@example.com") }
+
+    describe "for non-signed-in users" do
+
+      describe "in the Users controller" do
+        before { visit edit_user_path(user) }
+        it { should have_title('Sign in') }
+      end
+
+      describe "submitting to the update action" do
+        before { patch user_path(user) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+              expect(page).to have_title('Edit user')
+          end
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+
+        before do
+          visit edit_user_path(wrong_user)
+        end
+
+        specify { expect(response).to redirect_to(root_path) }
+      end
+
+      describe "submitting a PATCH to the Users#update action" do
+
+        before do
+          patch user_path(wrong_user)
+        end
+
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
+ 
   end
 end
