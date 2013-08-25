75a69f6b5787ad3489abef851e1476a37a34b233
Paul David Salcido <paulsalcido.79@gmail.com>
2013-08-25 01:53:15 +0000

    Users list index
    Fake users for testing and actual test database while running.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'less-rails'
 gem 'therubyracer'
 gem 'twitter-bootstrap-rails'
 gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'faker', '1.1.2'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,7 +27,9 @@ describe "AuthenticationPages" do
       end
 
       it { should have_title(user.name) }
+      it { should have_link("Users",        href: users_path) }
       it { should have_link("Profile",      href: user_path(user)) }
+      it { should have_link("Settings",     href: edit_user_path(user)) }
       it { should have_link("Sign out",     href: signout_path) }
       it { should_not have_link("Sign in",  href: signin_path) }
 
@@ -47,6 +49,11 @@ describe "AuthenticationPages" do
       describe "in the Users controller" do
         before { visit edit_user_path(user) }
         it { should have_title('Sign in') }
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe "submitting to the update action" do
