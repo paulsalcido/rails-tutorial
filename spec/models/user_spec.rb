47e80fede1caa28f781dde0c14e7cf6264b0b0e5
Paul Salcido <paulsalcido.79@gmail.com>
2013-09-08 18:09:18 +0000

    Micropost ordering is now correct.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -112,4 +112,18 @@ describe User do
     end
   end
 
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the correct order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
+
 end
